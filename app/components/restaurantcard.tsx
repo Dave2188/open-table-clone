d3e9ea58911e8aa5b4d799d21bf3a2cbc4dcce0e
David fox <davidfox967@yahoo.com>
2023-04-21 16:55:09 -0600

    star rating and utils

--- app/components/restaurantcard.tsx
+++ app/components/restaurantcard.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import { RestaurantCardType } from "../page";
+import Stars from "./stars";
 import Price from "./price";
+import { PRICE } from "@prisma/client";
 
-export default function Card({ restaurant, reviews }: { restaurant: RestaurantCardType; reviews: thisReviews[] }) {
+import { calculateReviewRatingAverage } from "../utilities/calculateReviewRatingAverage";
+
+export default function Card({ restaurant, reviews }: { restaurant: thisRestaurant; reviews: thisReviews[] }) {
 	return (
 		<>
 			<Link href={`/restaurant/${restaurant.slug}`}>
@@ -12,8 +15,10 @@ export default function Card({ restaurant, reviews }: { restaurant: RestaurantCa
 					<div className="p-1">
 						<h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
 						<div className="flex items-start">
-							<div className="flex mb-2 ">{starRating(reviews)} stars</div>
-							<p className="ml-2">{restaurant.reviews.length} reviews</p>
+							<Stars reviews={restaurant.reviews} />
+							<p className="ml-2">
+								{restaurant.reviews.length} {restaurant.reviews.length === 1 ? "review" : "reviews"}
+							</p>
 						</div>
 						<div className="flex text-reg font-light capitalize">
 							<p className=" mr-3">{restaurant.cuisine.name}</p>
@@ -28,14 +33,27 @@ export default function Card({ restaurant, reviews }: { restaurant: RestaurantCa
 	);
 }
 
-type thisReviews = {
+type thisRestaurant = {
+	reviews: thisReviews[];
+	id: number;
+	cuisine: { id: number; name: string };
+	price: PRICE;
+	location: {
+		id: number;
+		name: string;
+	};
+	name: string;
+	main_img: string;
+	slug: string;
+};
+
+export type thisReviews = {
 	id: number;
 	first_name: string;
 	last_name: string;
 	text: string;
 	rating: number;
 	restaurant_id: number;
-	user_id: number;
 };
 export const starRating = (reviews: thisReviews[]) => {
 	let sum = 0;
